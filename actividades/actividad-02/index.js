9cf1a6d89d5e6761f9beaa6fd7c0ccc58d799793
Jason Gomez <jasongomezmancha@gmail.com>
2026-09-05 09:55:44 -0500

    agregamos actividad 2 escritura con streams

---
+++ actividades/actividad-02/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+const writable = fs.createWriteStream('salida.txt');
+writable.write('Este es un mensaje de prueba.\n');
+writable.end('Fin del mensaje.');
+writable.on('finish', () => console.log('Escritura completada.'));
